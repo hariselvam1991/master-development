7b6829a60ae3c8640cc3f3f31ea4470a12566ce0
HA270519 <hariharasubramaniyam.selvam@wipro.com>
2020-12-05 15:55:24 +0530

    Commit by B fir subtract

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+package com.javabank;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		System.out.println(subtract(5,5));
+	}
+	
+	static int subtract(int a, int b) {
+		return a-b;
+	}
+
+}
